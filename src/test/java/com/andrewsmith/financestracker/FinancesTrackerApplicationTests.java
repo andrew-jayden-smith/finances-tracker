015eed1428c50adb061b04c52c96032e939a56c7
Andrew Smith <smithdrew867@gmail.com>
2026-01-22 12:40:44 -0500

    Initial Commit - Finances tracker

---
+++ src/test/java/com/andrewsmith/financestracker/FinancesTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrewsmith.financestracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancesTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
