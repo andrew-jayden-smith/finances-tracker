ad1d330f79ab03da8e85163581d6fcfec2567e34
Andrew Smith <smithdrew867@gmail.com>
2026-02-04 17:33:38 -0500

    Account Model cleaned up

--- src/main/java/com/andrewsmith/financestracker/model/Account.java
+++ src/main/java/com/andrewsmith/financestracker/model/Account.java
@@ -12,44 +12,31 @@ public class Account {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 50)
     private String name;
 
     @Column(nullable = false)
     private BigDecimal openingBalance;
 
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     private LocalDateTime openingDate;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-
     public Account() {}
 
     public Account(String name, BigDecimal openingBalance, LocalDateTime openingDate, User user) {
         this.name = name;
         this.openingBalance = openingBalance;
-        this.openingDate = openingDate;
         this.user = user;
-    }
-
-    public Account(Long id, String name, BigDecimal openingBalance, LocalDateTime openingDate, User user) {
-        this.id = id;
-        this.name = name;
-        this.openingBalance = openingBalance;
         this.openingDate = openingDate;
-        this.user = user;
     }
 
     // Getters and Setters
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getName() {
@@ -71,7 +58,6 @@ public class Account {
     public LocalDateTime getOpeningDate() {
         return openingDate;
     }
-
     public void setOpeningDate(LocalDateTime openingDate) {
         this.openingDate = openingDate;
     }
@@ -79,7 +65,6 @@ public class Account {
     public User getUser() {
         return user;
     }
-
     public void setUser(User user) {
         this.user = user;
     }
