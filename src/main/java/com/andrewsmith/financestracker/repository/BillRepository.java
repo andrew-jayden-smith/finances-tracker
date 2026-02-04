b4703a7932e4d3bb29be3fd97828ed395f96db0e
Andrew Smith <smithdrew867@gmail.com>
2026-02-03 19:26:14 -0500

    Bug fix with function name in BillRepo

--- src/main/java/com/andrewsmith/financestracker/repository/BillRepository.java
+++ src/main/java/com/andrewsmith/financestracker/repository/BillRepository.java
@@ -4,14 +4,16 @@ import com.andrewsmith.financestracker.model.Bill;
 import com.andrewsmith.financestracker.model.User;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 public interface BillRepository extends CrudRepository<Bill, Long>, JpaSpecificationExecutor<Bill> {
     List<Bill> findByUser(User user);
     // Check if bill has been paid
-    List<Bill> findByUserAndNextDueDateBetween(User user, LocalDate start, LocalDate end);
+    List<Bill> findBillsByStatus(User user, LocalDate start, LocalDate end);
 
     Bill findByUserAndNameIgnoreCase(User user, String name);
  }
